fdac15214c213b83d58dcaae8781c56ed6e8334e
pdao1 <pdao@revacomm.com>
2021-11-23 00:06:45 -1000

    Added feed, edit, update, and delete features

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,18 +8,46 @@ class PostsController < ApplicationController
     end
 
     def new
-
+        @post = Post.new
     end
 
     def create
         @post = Post.new(post_params)
 
-        @post.save
-        redirect_to @post
+        if(@post.save)
+            redirect_to @post
+        else 
+            render 'new'
+        end
+    end
+
+    def edit 
+        @post = Post.find(params[:id])
+    end
+
+    def update 
+        @post = Post.find(params[:id])
+
+        if(@post.update(post_params))
+            redirect_to @post
+        else 
+            render 'edit'
+        end
+    end
+
+    def destroy
+        @post = Post.find(params[:id])
+        @post.destroy
+
+        redirect_to posts_path
+    end
+
+    def post
+        params.require(:post).permit(:title, :body, :picture, :image, :attached)
     end
 
     private def post_params
-        params.require(:post).permit(:title, :body, :picture)
+        params.require(:post).permit(:title, :body, :picture, :image, :attached)
     end
 
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ApplicationRecord
+    validates :title, presence: true,
+                      length: {minimum: 5}
+    has_one_attached :picture
 end
